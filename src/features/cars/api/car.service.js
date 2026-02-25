f52ab3805d7080221897967f50c9b579562f3fab
fakherjn123 <fakher.jn2004@gmail.com>
2026-02-25 02:28:08 +0100

    Corrige quelque erreurs

--- src/features/cars/api/car.service.js
+++ src/features/cars/api/car.service.js
@@ -2,6 +2,3 @@ import api from "../../../config/api.config";
 
 export const getCarsService = () =>
   api.get("/cars");
-
-export const getCarByIdService = (id) =>
-  api.get(`/cars/${id}`);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRouter from "./app/router";
+import Router from "./app/router";
 import { AuthProvider } from "./features/auth/context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <AppRouter />
+    <Router/>
   </AuthProvider>
 );
